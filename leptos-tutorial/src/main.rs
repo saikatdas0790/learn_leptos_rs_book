7b12761cf827518c25cf148d8a38a1bbe598c04c
Saikat Das <saikatdas0790@gmail.com>
2023-10-11 23:14:34 +0530

    3.4. Iteration

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -13,6 +13,8 @@ fn App() -> impl IntoView {
         <button on:click=move |_| set_count.update(|n| *n += 1)>"Click me: " {count}</button>
         <ProgressBar progress=count/>
         <ProgressBar progress=Signal::derive(double_count)/>
+        <StaticList/>
+        <DynamicList initial_length=5/>
     }
 }
 
@@ -27,4 +29,70 @@ fn ProgressBar(
     progress: Signal<i32>,
 ) -> impl IntoView {
     view! { <progress max=max value=progress></progress> }
+}
+
+#[component]
+fn StaticList() -> impl IntoView {
+    let length = 5;
+    let counters = (1..=length).map(create_signal);
+
+    let counter_buttons = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button on:click=move |_| set_count.update(|n| *n += 1)>{count}</button>
+                </li>
+            }
+        })
+        .collect_view();
+
+    view! { <ul>{counter_buttons}</ul> }
+}
+
+#[component]
+fn DynamicList(initial_length: usize) -> impl IntoView {
+    let mut next_counter_id = initial_length;
+
+    let initial_counters = (0..initial_length)
+        .map(|id| (id, create_signal(id + 1)))
+        .collect::<Vec<_>>();
+
+    let (counters, set_counters) = create_signal(initial_counters);
+
+    let add_counter = move |_| {
+        let sig = create_signal(next_counter_id + 1);
+        set_counters.update(move |counters| counters.push((next_counter_id, sig)));
+        next_counter_id += 1;
+    };
+
+    view! {
+        <div>
+            <button on:click=add_counter>"Add Counter"</button>
+            <ul>
+                <For
+                    each=counters
+                    key=|counter| counter.0
+                    children=move |(id, (count, set_count))| {
+                        view! {
+                            <li>
+                                <button on:click=move |_| {
+                                    set_count.update(|n| *n += 1)
+                                }>{count}</button>
+                                <button on:click=move |_| {
+                                    set_counters
+                                        .update(|counters| {
+                                            counters.retain(|(counter_id, _)| counter_id != &id)
+                                        });
+                                }>
+
+                                    "Remove"
+                                </button>
+                            </li>
+                        }
+                    }
+                />
+
+            </ul>
+        </div>
+    }
 }
