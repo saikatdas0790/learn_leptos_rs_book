9b11e5b04a9e82a66a30acc0ad6d28ddb665a3ac
Saikat Das <saikatdas0790@gmail.com>
2023-10-11 21:49:44 +0530

    3.3. Components and Props

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -7,27 +7,24 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
-    let (x, set_x) = create_signal(0);
-    let (y, set_y) = create_signal(0);
     let double_count = move || count() * 2;
 
     view! {
-        <div
-            style="position:absolute"
-            style:left=move || format!("{}px", x() + 100)
-            style:top=move || format!("{}px", y() + 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", x(), y())
-            style=("--columns", x)
-        >
-            "Moves when coordinates change"
-        </div>
-
-        <progress max="50" value=double_count></progress>
-        <p>"Double Count:" {double_count}</p>
-
-        <button on:click=move |_| set_count.update(|n| *n += 1) class:red=move || count() % 2 == 1>
-            "Click me: "
-            {count}
-        </button>
+        <button on:click=move |_| set_count.update(|n| *n += 1)>"Click me: " {count}</button>
+        <ProgressBar progress=count/>
+        <ProgressBar progress=Signal::derive(double_count)/>
     }
+}
+
+/// Shows progress toward a goal.
+#[component]
+fn ProgressBar(
+    /// The maximum value of the progress bar.
+    #[prop(default = 100)]
+    max: u16,
+    /// How much progress should be displayed.
+    #[prop(into)]
+    progress: Signal<i32>,
+) -> impl IntoView {
+    view! { <progress max=max value=progress></progress> }
 }
