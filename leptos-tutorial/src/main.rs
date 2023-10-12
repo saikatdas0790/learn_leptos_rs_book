8b6857739f114a57c2efe26d39ff5ff144744d3d
Saikat Das <saikatdas0790@gmail.com>
2023-10-12 22:00:27 +0530

    3.5. Forms and Inputs

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,4 +1,5 @@
-use leptos::*;
+use leptos::{html::Input, *};
+use web_sys::SubmitEvent;
 
 fn main() {
     mount_to_body(|| view! { <App/> })
@@ -13,7 +14,33 @@ fn App() -> impl IntoView {
 }
 
 #[component]
-fn ControlledComponent() -> impl IntoView {}
+fn ControlledComponent() -> impl IntoView {
+    let (name, set_name) = create_signal("Controlled".to_string());
+
+    view! {
+        <input type="text" on:input=move |ev| set_name(event_target_value(&ev)) prop:value=name/>
+        <p>"Name is: " {name}</p>
+    }
+}
 
 #[component]
-fn UncontrolledComponent() -> impl IntoView {}
+fn UncontrolledComponent() -> impl IntoView {
+    let (name, set_name) = create_signal("Uncontrolled".to_string());
+
+    let input_element: NodeRef<Input> = create_node_ref();
+
+    let on_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+
+        let value = input_element().expect("<input> element to exist").value();
+        set_name(value);
+    };
+
+    view! {
+        <form on:submit=on_submit>
+            <input type="text" value=name node_ref=input_element/>
+            <input type="submit" value="Submit"/>
+        </form>
+        <p>"Name is: " {name}</p>
+    }
+}
