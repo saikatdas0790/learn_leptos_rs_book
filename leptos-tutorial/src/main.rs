4ff883c439ad42673175065556230d70d178a7ac
Saikat Das <saikatdas0790@gmail.com>
2023-10-17 21:23:00 +0530

    3.7. Error Handling

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -6,40 +6,35 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (value, set_value) = create_signal(0);
-    let is_odd = move || value() & 1 == 1;
-    let odd_text = move || if is_odd() { Some("How odd!") } else { None };
+    let (value, set_value) = create_signal(Ok(0));
+
+    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
 
     view! {
-        <h1>Control Flow</h1>
-        <button on:click=move |_| set_value.update(|n| *n += 1)>"+1"</button>
-        <p>"Value is: " {value}</p>
+        <h1>"Error Handling"</h1>
+        <label>
+            "Type a number (or something that's not a number!)"
+            <input type="number" on:input=on_input/>
+            <ErrorBoundary fallback=|errors| {
+                view! {
+                    <div class="error">
+                        <p>"Not a number! Errors: "</p>
+                        <ul>
+                            {move || {
+                                errors
+                                    .get()
+                                    .into_iter()
+                                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                                    .collect::<Vec<_>>()
+                            }}
 
-        <hr/>
+                        </ul>
+                    </div>
+                }
+            }>
 
-        <h2>
-            <code>"Option<T>"</code>
-        </h2>
-        <p>{odd_text}</p>
-        <p>{move || odd_text().map(|text| text.len())}</p>
-
-        <h2>"Conditional Logic"</h2>
-        <p>{move || if is_odd() { "Odd" } else { "Even" }}</p>
-
-        <p class:hidden=is_odd>"Appears if even."</p>
-
-        <Show when=is_odd fallback=|| view! { <p>"Even steven"</p> }>
-            <p>"Oddment"</p>
-        </Show>
-
-        {move || is_odd().then(|| view! { <p>"Oddity"</p> })}
-
-        <h2>"Converting between Types"</h2>
-
-        {move || match is_odd() {
-            true if value() == 1 => view! { <pre>"One"</pre> }.into_any(),
-            false if value() == 2 => view! { <p>"Two"</p> }.into_any(),
-            _ => view! { <textarea>{value()}</textarea> }.into_any(),
-        }}
+                <p>"You entered " <strong>{value}</strong></p>
+            </ErrorBoundary>
+        </label>
     }
 }
