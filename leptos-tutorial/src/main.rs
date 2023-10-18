0b93b43aab820928273429b2f3ebc9dcbff6e048
Saikat Das <saikatdas0790@gmail.com>
2023-10-18 20:53:24 +0530

    3.3. Components and Props

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -11,16 +11,18 @@ fn App() -> impl IntoView {
     let double_count = move || count() * 2;
 
     view! {
-        <button on:click=move |_| set_count.update(|n| *n += 1) class:red=move || count() % 2 == 1>
-            "Click me"
-        </button>
+        <button on:click=move |_| { set_count.update(|n| *n += 1) }>"Click me"</button>
         <br/>
-
-        <progress max="50" value=count></progress>
-        <br/>
-
-        <progress max="50" value=double_count></progress>
-        <p>"Count: " {count}</p>
-        <p>"Double Count: " {double_count}</p>
+        <ProgressBar max=50 progress=count/>
+        <ProgressBar progress=count/>
+        <ProgressBar max=50 progress=Signal::derive(double_count)/>
     }
+}
+
+#[component]
+fn ProgressBar(
+    #[prop(default = 100)] max: u16,
+    #[prop(into)] progress: Signal<i32>,
+) -> impl IntoView {
+    view! { <progress max=max value=progress></progress> }
 }
