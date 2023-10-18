53014a6f08ba5a8c33eebec1e965e38203bbd3bc
Saikat Das <saikatdas0790@gmail.com>
2023-10-18 20:07:56 +0530

    3.1. A Basic Component

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -6,35 +6,21 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (value, set_value) = create_signal(Ok(0));
-
-    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
+    let (count, set_count) = create_signal(0);
 
     view! {
-        <h1>"Error Handling"</h1>
-        <label>
-            "Type a number (or something that's not a number!)"
-            <input type="number" on:input=on_input/>
-            <ErrorBoundary fallback=|errors| {
-                view! {
-                    <div class="error">
-                        <p>"Not a number! Errors: "</p>
-                        <ul>
-                            {move || {
-                                errors
-                                    .get()
-                                    .into_iter()
-                                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
-                                    .collect::<Vec<_>>()
-                            }}
-
-                        </ul>
-                    </div>
-                }
-            }>
-
-                <p>"You entered " <strong>{value}</strong></p>
-            </ErrorBoundary>
-        </label>
+        <button on:click=move |_| { set_count.update(|n| *n += 2) }>"Click me"</button>
+        <p>
+            <strong>"Reactive: "</strong>
+            {move || count.get()}
+        </p>
+        <p>
+            <strong>"Reactive shorthand: "</strong>
+            {count}
+        </p>
+        <p>
+            <strong>"Not reactive: "</strong>
+            {count()}
+        </p>
     }
 }
