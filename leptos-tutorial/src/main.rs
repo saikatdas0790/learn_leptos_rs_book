3732b2bf84c8a6093c86b4b4a89fb1c7e5acba8c
Saikat Das <saikatdas0790@gmail.com>
2023-10-21 11:24:59 +0530

    3.8. Parent-Child Communication

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use web_sys::MouseEvent;
 
 fn main() {
     mount_to_body(|| view! { <App/> })
@@ -6,34 +7,58 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (value, set_value) = create_signal(Ok(0));
+    let (red, set_red) = create_signal(false);
+    let (right, set_right) = create_signal(false);
+    let (italics, set_italics) = create_signal(false);
+    let (smallcaps, set_smallcaps) = create_signal(false);
 
-    let on_input = move |ev| set_value(event_target_value(&ev).parse());
+    provide_context(SmallcapsContext(set_smallcaps));
 
     view! {
-        <h1>"Error Handling"</h1>
-        <label>
-            "Type a number (or something that's not a number!)"
-            <input type="text" on:input=on_input/>
-            <ErrorBoundary fallback=|errors| {
-                view! {
-                    <div class="error">
-                        <p>"Not a number! Errors: "</p>
-                        <ul>
-                            {move || {
-                                errors
-                                    .get()
-                                    .into_iter()
-                                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
-                                    .collect::<Vec<_>>()
-                            }}
+        <main>
+            <p class:red=red class:right=right class:italics=italics class:smallcaps=smallcaps>
+                "Lorem ipsum sit dolor amet."
+            </p>
 
-                        </ul>
-                    </div>
-                }
-            }>
-                <p>"You entered " <strong>{value}</strong></p>
-            </ErrorBoundary>
-        </label>
+            <ButtonA setter=set_red/>
+
+            <ButtonB on_click=move |_| set_right.update(|value| *value = !*value)/>
+
+            <ButtonC on:click=move |_| set_italics.update(|value| *value = !*value)/>
+
+            <ButtonD/>
+        </main>
+    }
+}
+
+#[derive(Clone, Copy)]
+struct SmallcapsContext(WriteSignal<bool>);
+
+#[component]
+pub fn ButtonA(setter: WriteSignal<bool>) -> impl IntoView {
+    view! { <button on:click=move |_| setter.update(|value| *value = !*value)>"Toggle Red"</button> }
+}
+
+#[component]
+pub fn ButtonB<F>(on_click: F) -> impl IntoView
+where
+    F: Fn(MouseEvent) + 'static,
+{
+    view! { <button on:click=on_click>"Toggle Right"</button> }
+}
+
+#[component]
+pub fn ButtonC() -> impl IntoView {
+    view! { <button>"Toggle Italics"</button> }
+}
+
+#[component]
+pub fn ButtonD() -> impl IntoView {
+    let setter = use_context::<SmallcapsContext>().unwrap().0;
+
+    view! {
+        <button on:click=move |_| {
+            setter.update(|value| *value = !*value)
+        }>"Toggle Smallcaps"</button>
     }
 }
