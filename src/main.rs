cef4db34f43a9207e31239c0124a308f395c3f6e
Saikat Das <saikatdas0790@gmail.com>
2025-05-28 17:00:57 +0000

    feat: add logging support with console_log and console_error_panic_hook

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use leptos::prelude::*;
 
 fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
     leptos::mount::mount_to_body(|| {
         view! {
             <p>"Hello, world!"</p>
