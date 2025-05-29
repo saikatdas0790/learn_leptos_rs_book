ce3a7236386955899fcb74f9403fa7f67466460e
Saikat Das <saikatdas0790@gmail.com>
2025-05-29 06:11:42 +0000

    feat: refactor main function to use App component for rendering and update settings for Emmet language support

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,29 @@ use leptos::prelude::*;
 fn main() {
     let _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| {
-        view! {
-            <p>"Hello, world!"</p>
-        }
-    });
+    leptos::mount::mount_to_body(App);
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button on:click=move |_| {
+            *set_count.write() += 1;
+        }>"Click me: " {count}</button>
+        <p>"Double count: " {move || count.get() * 2}</p>
+        <p>
+            <strong>"Reactive: "</strong>
+            {move || count.get()}
+        </p>
+        <p>
+            <strong>"Reactive shorthand: "</strong>
+            {count}
+        </p>
+        <p>
+            <strong>"Not reactive: "</strong>
+            {count.get()}
+        </p>
+    }
 }
