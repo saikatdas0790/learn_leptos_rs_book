3f52c4f31c1407e841b92341dad3be3b0073b4ef
Saikat Das <saikatdas0790@gmail.com>
2025-05-29 12:12:05 +0000

    feat: update App component to increase button click increment and enhance styling

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,27 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
+    let double_count = move || count.get() * 2;
+    let html = "<p>This HTML will be injected.</p>";
 
     view! {
         <button
             on:click=move |_| {
-                *set_count.write() += 1;
+                *set_count.write() += 10;
             }
-            class:red=move || count.get() % 2 == 1
+            style="position: absolute"
+            style:left=move || { format!("{}px", count.get() + 100) }
+            style:background-color=move || format!("rgb({}, {}, 100)", count.get(), 100)
+            style:max-width="400px"
+            style=("--columns", move || count.get().to_string())
         >
             "Click me: "
             {count}
         </button>
+
+        <br />
+        <progress max="50" value=double_count></progress>
+        <p>"Double count: " {double_count}</p>
+        <div inner_html=html />
     }
 }
