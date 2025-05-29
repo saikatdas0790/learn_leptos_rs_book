4a7e27a603952ed43d3ca9490cdf84a78b53014e
Saikat Das <saikatdas0790@gmail.com>
2025-05-29 12:17:44 +0000

    feat: update App component to modify button click increment and enhance progress display

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,26 @@ fn main() {
 fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
     let double_count = move || count.get() * 2;
-    let html = "<p>This HTML will be injected.</p>";
 
     view! {
         <button
             on:click=move |_| {
-                *set_count.write() += 10;
+                *set_count.write() += 1;
             }
-            style="position: absolute"
-            style:left=move || { format!("{}px", count.get() + 100) }
-            style:background-color=move || format!("rgb({}, {}, 100)", count.get(), 100)
-            style:max-width="400px"
-            style=("--columns", move || count.get().to_string())
+            class:red=move || count.get() % 2 == 1
+            class=("button-20", move || count.get() % 2 == 1)
         >
             "Click me: "
-            {count}
         </button>
 
         <br />
+
+        <progress max="50" value=count></progress>
+
+        <br />
+
         <progress max="50" value=double_count></progress>
+        <p>"Count: " {count}</p>
         <p>"Double count: " {double_count}</p>
-        <div inner_html=html />
     }
 }
