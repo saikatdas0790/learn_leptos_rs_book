97e9b724a123a998de5e845b020166422e95fdab
Saikat Das <saikatdas0790@gmail.com>
2025-05-29 11:54:07 +0000

    feat: update App component to enhance button interactivity and style

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,14 @@ fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <button on:click=move |_| {
-            *set_count.write() += 1;
-        }>"Click me: " {count}</button>
-        <p>"Double count: " {move || count.get() * 2}</p>
-        <p>
-            <strong>"Reactive: "</strong>
-            {move || count.get()}
-        </p>
-        <p>
-            <strong>"Reactive shorthand: "</strong>
+        <button
+            on:click=move |_| {
+                *set_count.write() += 1;
+            }
+            class:red=move || count.get() % 2 == 1
+        >
+            "Click me: "
             {count}
-        </p>
-        <p>
-            <strong>"Not reactive: "</strong>
-            {count.get()}
-        </p>
+        </button>
     }
 }
