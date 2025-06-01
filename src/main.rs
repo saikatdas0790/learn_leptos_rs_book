02b05f13a48ce634095c82b2593a68a02801a2cc
Saikat Das <saikatdas0790@gmail.com>
2025-06-01 14:50:59 +0000

    refactor: remove ProgressBar component and integrate StaticList and DynamicList components in App

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::marker::PhantomData;
-
 use leptos::prelude::*;
 
 fn main() {
@@ -10,26 +8,72 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
-    let double_count = move || *count.read() * 2;
-
     view! {
-        <button on:click=move |_| {
-            *set_count.write() += 1;
-        }>"Click me: "</button>
-        <ProgressBar progress=count />
-        <ProgressBar progress=double_count />
+        <h1>"Iteration"</h1>
+        <h2>"Static List"</h2>
+        <p>"Use this pattern if the list itself is static."</p>
+        <StaticList length=5></StaticList>
+        <h2>"Dynamic List"</h2>
+        <p>"Use this pattern if the rows in your list will change"</p>
+        <DynamicList initial_length=5></DynamicList>
     }
 }
 
-/// Shows progress towards a goal.
 #[component]
-fn ProgressBar(
-    /// The maximum value of the progress bar.
-    #[prop(default = 100)]
-    max: u16,
-    /// How much progress should be displayed.
-    progress: impl Fn() -> i32 + Send + 'static,
-) -> impl IntoView {
-    view! { <progress max=max value=progress style:display="block" style:margin-top="1rem"></progress> }
+fn StaticList(length: usize) -> impl IntoView {
+    let counters = (1..=length).map(|idx| RwSignal::new(idx));
+
+    let counter_buttons: Vec<_> = counters
+        .map(|count| {
+            view! {
+                <li>
+                    <button on:click=move |_| *count.write() += 1>{count}</button>
+                </li>
+            }
+        })
+        .collect();
+
+    view! { <ul>{counter_buttons}</ul> }
+}
+
+#[component]
+fn DynamicList(initial_length: usize) -> impl IntoView {
+    let mut next_counter_id = initial_length;
+
+    let initial_counters = (0..initial_length)
+        .map(|id| (id, ArcRwSignal::new(id + 1)))
+        .collect::<Vec<_>>();
+
+    let (counters, set_counters) = signal(initial_counters);
+
+    let add_counter = move |_| {
+        let sig = ArcRwSignal::new(next_counter_id + 1);
+        set_counters.update(move |counters| {
+            counters.push((next_counter_id, sig));
+        });
+        next_counter_id += 1;
+    };
+
+    view! {
+        <button on:click=add_counter>"Add Counter"</button>
+        <ul>
+            <For
+                each=move || counters.get()
+                key=|counter| counter.0
+                children=move |(id, count)| {
+                    let count = RwSignal::from(count);
+                    view! {
+                        <li>
+                            <button on:click=move |_| *count.write() += 1>{count}</button>
+                            <button on:click=move |_| {
+                                set_counters
+                                    .write()
+                                    .retain(|(counter_id, _)| { counter_id != &id });
+                            }>"Remove"</button>
+                        </li>
+                    }
+                }
+            ></For>
+        </ul>
+    }
 }
